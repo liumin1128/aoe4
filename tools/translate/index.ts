8890218c90585507d0a44ce8f868e06ff83a8aff
Min_Liu <Min_Liu@singaporeair.com.sg>
2024-11-20 18:49:47 +0800

    feat: translate

--- tools/translate/index.ts
+++ tools/translate/index.ts
@@ -1,3 +1,42 @@
-import { buildings } from "@data/sdk";
+import { civilizations, units } from "@data/sdk";
+import "./openai";
 
-console.log(buildings);
+interface Translation {
+  name?: string;
+  description?: string;
+}
+
+// const civsTrans: Translation[] = civilizations.list.map((civ) => {
+//   return {
+//     name: civ.name,
+//     description: civ.description,
+//   };
+// });
+
+const unitsTrans: Translation[] = units
+  // .where({ civilization: "ab" })
+  // .filter((i) => i.civs.length === 1)
+  .order("hitpoints", "age")
+  .map((unit) => {
+    return {
+      name: unit.name,
+      description: unit.description,
+    };
+  })
+  .splice(0, 10);
+
+console.log(unitsTrans);
+
+// async function test() {
+//   console.log("llm");
+//   console.log(llm);
+
+//   const aiMsg = await llm.invoke([
+//     ["system", ""],
+//     ["human", "讲个笑话"],
+//   ]);
+//   console.log("aiMsg.content");
+//   console.log(aiMsg.content);
+// }
+
+// test();

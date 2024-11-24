2057efdab4c74bedc0d193697f1e79699a5fd27c
Min_Liu <Min_Liu@singaporeair.com.sg>
2024-11-24 17:48:33 +0800

    feat: update

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,7 +2,7 @@ import {NextIntlClientProvider} from 'next-intl';
 import {getMessages} from 'next-intl/server';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
-import "./globals.css";
+import "../global.css";
 
 interface Params {
   locale: string
